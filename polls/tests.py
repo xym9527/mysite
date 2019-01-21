5da26b5776e2eb87e7aeee8fce455e4718848694
xym9527 <yanmbcj@163.com>
2019-01-21 18:33:06 +0800

    trnk分支

--- polls/tests.py
+++ polls/tests.py
@@ -15,3 +15,21 @@ class QuestionModelTests(TestCase):
         time = timezone.now()+datetime.timedelta(days=2)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(),False,'{0} 测试用例不通过'.format(self.__class__.__name__))
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() returns True for questions whose pub_date
+        is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,get_object_or_404
 from django.http import  HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question,Choice
 
@@ -12,12 +13,18 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
-
+        now = timezone.now()
+        # return Question.objects.order_by('-pub_date')[:5]
+        return  Question.objects.filter(pub_date__lte=now).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     template_name = 'polls/detail.html'
     model = Question
+
+    def get_queryset(self):
+        now = timezone.now()
+        # return Question.objects.order_by('-pub_date')[:5]
+        return  Question.objects.filter(pub_date__lte=now)
 
 
 class ResultView(generic.DetailView):
