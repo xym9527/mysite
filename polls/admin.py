63349d074a1a19a01830e83c2cc931c87da07bca
xym9527 <yanmbcj@163.com>
2019-01-21 19:35:10 +0800

    trnk分支

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question,Choice
 
 # Register your models here.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    list_display = ['question_text','pub_date','was_published_recently']
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    inlines = [ChoiceInline]
+
+K
+admin.site.register(Question,QuestionAdmin)
+
